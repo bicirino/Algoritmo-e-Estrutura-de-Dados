169380f4d39b5b5cf73eba6b1a7dedba9ce07cd3
Gabriel Martins Cirino <gabrielcirinom@dinfor.uniceub>
2026-07-27 20:01:35 -0300

    Preenchimento Vetor

--- Aula01/PreenchimentoVetor.java
+++ Aula01/PreenchimentoVetor.java
@@ -1,8 +1,21 @@
+import java.util.Scanner; 
+
 public class PreenchimentoVetor { 
     public static void main(String[] args){ 
 
         Scanner scan = new Scanner(System.in); 
 
         int Vetor[] = new int[5]; 
+
+        
+        for (int i = 0; i < Vetor.length; i++){ 
+            System.out.print("Digite o valor da posição " + i + ": "); 
+            Vetor[i] = scan.nextInt(); 
+        }
+
+       
+        for (int j = 0; j < Vetor.length; j++){ 
+            System.out.println(Vetor[j]); 
+        }
     }
 }
